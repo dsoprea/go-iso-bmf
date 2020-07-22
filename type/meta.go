4cb817d83a745521a7fed039680f69f2173540bb
Dustin Oprea <dustin@randomingenuity.com>
2020-07-22 15:31:37 -0400

    type: Add meta type. Needs tests.
    This exposes a bunch of other boxes that we will have to deal with in
    order to extract the EXIF blob.

---
+++ type/meta.go
@@ -0,0 +1,60 @@
+package bmftype
+
+import (
+	"github.com/dsoprea/go-logging"
+
+	"github.com/dsoprea/go-iso-bmf/common"
+)
+
+// MetaBox is a "Track" box.
+type MetaBox struct {
+	bmfcommon.Box
+
+	// LoadedBoxIndex contains this boxes children.
+	bmfcommon.LoadedBoxIndex
+}
+
+type metaBoxFactory struct {
+}
+
+// Name returns the name of the type.
+func (metaBoxFactory) Name() string {
+
+	// TODO(dustin): Add test
+
+	return "meta"
+}
+
+// New returns a new value instance.
+func (metaBoxFactory) New(box bmfcommon.Box) (cb bmfcommon.CommonBox, err error) {
+	defer func() {
+		if errRaw := recover(); errRaw != nil {
+			err = log.Wrap(errRaw.(error))
+		}
+	}()
+
+	// TODO(dustin): Add test
+
+	// Boxes follow the four-bytes with the version/flags.
+
+	boxes, err := box.ReadBoxes(4)
+	log.PanicIf(err)
+
+	index := boxes.Index()
+
+	metaBox := &MetaBox{
+		Box:            box,
+		LoadedBoxIndex: index,
+	}
+
+	return metaBox, nil
+}
+
+var (
+	_ bmfcommon.BoxFactory = metaBoxFactory{}
+	_ bmfcommon.CommonBox  = &MetaBox{}
+)
+
+func init() {
+	bmfcommon.RegisterBoxType(metaBoxFactory{})
+}
